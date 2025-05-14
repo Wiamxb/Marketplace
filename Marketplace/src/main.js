c28fd9543186295ee8388dbfe5fef053ce970b66
Wiamxb <bolawiam@gmail.com>
2025-05-14 18:29:38 +0200

    DAG 3

--- Marketplace/src/main.js
+++ Marketplace/src/main.js
@@ -4,36 +4,82 @@ const toonWinkelmandKnop = document.getElementById('toon-winkelmand');
 const toonFavorietenKnop = document.getElementById('toon-favorieten');
 
 let alleProducten = [];
-
-// Haalt producten op van de API
+let favorieten = JSON.parse(localStorage.getItem('favorieten')) || [];
+let winkelmand = [];
+/*API*/
 async function haalProductenOp() {
   const antwoord = await fetch('https://dummyjson.com/products');
   const gegevens = await antwoord.json();
   return gegevens.products;
 }
-
-// Toont de producten op de pagina
+/*Producten tonen*/
 function toonProducten(producten) {
+  const productLijst = document.getElementById('productLijst');
   productLijst.innerHTML = '';
-  producten.forEach((product) => {
-    const kaart = document.createElement('div');
-    kaart.classList.add('product-kaart');
-    kaart.innerHTML = `
+  producten.forEach(product => {
+    const isFavoriet = favorieten.includes(product.id);
+    const inWinkelmand = winkelmand.some(item => item.id === product.id);
+    productLijst.innerHTML += `
+    <div class="product-kaart">
       <img src="${product.thumbnail}" alt="${product.title}">
       <h3>${product.title}</h3>
       <p>${product.description}</p>
       <p>💰 ${product.price} €</p>
       <p>${product.category}</p>
-      <button>Toevoegen aan winkelmandje</button>
+      <button class="favoriet-knop" data-id="${product.id}">
+          ${isFavoriet ? '❤️ Verwijder uit favorieten' : '🤍 Voeg toe aan favorieten'}
+        </button>
+        <button class="winkelmand-knop" data-id="${product.id}">
+          Toevoegen aan winkelmandje
+        </button>
+      </div>
     `;
-    productLijst.appendChild(kaart);
   });
+  initFavorietenKnoppen ();
 }
 
-// Start het ophalen en tonen van producten
 haalProductenOp().then(producten => {
   alleProducten = producten;
   toonProducten(alleProducten);
 });
 
-/*-------------------------*/
+
+/*---------------------------------------------------------------------------------------------------------------*/
+/*code: zoekbalk*/
+zoekInvoer.addEventListener('input', () => {
+  const zoekterm = zoekInvoer.value.toLowerCase();
+  const gevondenProducten = alleProducten.filter(product =>
+    product.title.toLowerCase().includes(zoekterm) ||
+    product.description.toLowerCase().includes(zoekterm)
+  );
+  toonProducten(gevondenProducten);
+});
+
+/*code: filtreren*/ 
+document.querySelectorAll('.filter-knop').forEach(knop => {
+  knop.addEventListener('click', () => {
+    const categorie = knop.dataset.filter; 
+    const gefilterdeProducten = categorie === 'all' 
+    ? alleProducten
+    : alleProducten.filter(product => product.category === categorie); 
+    toonProducten(gefilterdeProducten); 
+  })
+})
+/*code: favorietenknop*/ 
+function initFavorietenKnoppen() {
+  document.querySelectorAll('.favoriet-knop').forEach(knop => {
+    knop.addEventListener('click', () => {
+      const id = parseInt(knop.dataset.id); 
+      if (favorieten.includes(id)) {
+        favorieten = favorieten.filter(fid => fid !== id);
+      } else {
+        favorieten.push(id);
+      }
+      localStorage.setItem('favorieten', JSON.stringify(favorieten));
+      updateFavorietenAantal(); 
+      toonProducten(alleProducten); 
+    });
+  });
+}
+
+
