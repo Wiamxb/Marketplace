f980879ab057a541b820d29ad5c74fb417286122
Wiamxb <bolawiam@gmail.com>
2025-05-20 17:09:07 +0200

    "Producten ophalen van API"

--- Marketplace/src/main.js
+++ Marketplace/src/main.js
@@ -1,3 +1,4 @@
+/*basisvariabelen*/
 let allProducts = [];
 let favourites = JSON.parse(localStorage.getItem('favourites')) || [];
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
@@ -6,3 +7,10 @@ const productList = document.getElementById('product-list');
 const searchInput = document.getElementById('search');
 const showCartButton = document.getElementById('show-cart');
 const cartContainer = document.getElementById('cart-container');
+
+/*Producten ophalen van API*/
+async function fetchProducts() {
+  const res = await fetch('https://dummyjson.com/products');
+  const data = await res.json();
+  return data.products;
+}
